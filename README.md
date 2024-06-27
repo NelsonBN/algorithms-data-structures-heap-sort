5ae4dbbf847125f46d1d4edf8ebcac4c133e950b
Nelson Nobre <technobre@gmail.com>
2024-06-27 01:54:23 +0100

    Kickoff

---
+++ README.md
@@ -0,0 +1,25 @@
+# Algorithms and Data Structures - Heap Sort
+
+
+## Characteristics
+- Time complexity:
+    - Best: Ω(n log(n))
+    - Average: Θ(n log(n))
+    - Worst: O(n log(n))
+- Space complexity: O(1)
+- In-place
+- Unstable
+
+
+## Demos:
+- [Basic Implementation](./src/basic.py)
+- [Optimized Comparisons](./src/optimized_comparisons.py)
+- [Without Recursion](./src/without_recursion.py)
+
+
+## Demonstration
+- [Algorithm Visualizer](https://algorithm-visualizer.org/brute-force/heapsort)
+
+
+## References
+- [Other algoritmos & Data Structures](https://github.com/NelsonBN/algorithms-data-structures)
---
+++ src/without_recursion.py
@@ -0,0 +1,32 @@
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        while True:
+            left = 2 * i + 1
+            right = 2 * i + 2
+            if left < n and arr[largest] < arr[left]:
+                largest = left
+            if right < n and arr[largest] < arr[right]:
+                largest = right
+            if largest != i:
+                arr[i], arr[largest] = arr[largest], arr[i]
+                i = largest
+            else:
+                break
+
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+#        0  1  2   3   4  5  6   7  8   9   10  11 12  13
+array = [5, 3, 21, 13, 1, 6, 15, 8, 33, 17, 18, 2, 19, 14]
+
+print("Before: ", array)
+heap_sort(array)
+print("After: ", array)
